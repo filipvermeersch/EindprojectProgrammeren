f9434fa8e3f7b72c0dcedf86998b9c47b29aca79
filip.vermeersch <filip.vermeersch@outlook.com>
2018-03-18 08:00:49 +0100

    Wedstrijdcontroler - wedstrijdVM - begin wedstrijdpagina met Vue

--- Projectwerk.Vermeersch.f/Controllers/WedstrijdenController.cs
+++ Projectwerk.Vermeersch.f/Controllers/WedstrijdenController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Projectwerk.Vermeersch.f.Data;
+using Projectwerk.Vermeersch.f.Models;
+using Projectwerk.Vermeersch.f.Viewmodels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,45 @@ namespace Projectwerk.Vermeersch.f.Controllers
 {
     public class WedstrijdenController : Controller
     {
+        private TriatlonContext db = new TriatlonContext();
+
         // GET: Wedstrijden
-        public ActionResult Index()
+        public ActionResult Index(int Id)
         {
-            return View();
+            var putter = db.Putters.Include("Wedstrijden").Include("Resultaten").FirstOrDefault(p => p.Id == Id);
+            var wedstrijdlijst = putter.Wedstrijden;
+            var resultaten = putter.Resultaten;
+
+            var wedstrijden = new List<WedstrijdViewModel>();
+            foreach ( var wedstrijd in putter.Wedstrijden )
+            {
+                WedstrijdViewModel WVM = new WedstrijdViewModel();
+                WVM.PutterId = Id;
+                WVM.Putternaam = putter.VolledigeNaam;
+                WVM.Plaats = wedstrijd.Plaats;
+                WVM.Datum = wedstrijd.Datum;
+                WVM.Afstand = wedstrijd.Afstand.Lengte;
+                WVM.Sport = wedstrijd.Sport.soortSport;
+                wedstrijden.Add(WVM);
+            }
+
+            return View(wedstrijden);
+        }
+        // public JsonResult test(int Id)
+        //{
+        //    var testje = new List<Test>() { new Test { id = 1, testnaam = "vermeersch", testvoornaam = "fluppe" },
+        //        new Test { id = 2, testnaam = "hendrickx", testvoornaam = "sven" } };
+        //    return Json(testje, JsonRequestBehavior.AllowGet);
+
+        //}
+
+        public ActionResult test(int Id)
+        {
+            var testje = new List<Test>() { new Test { id = 1, testnaam = "vermeersch", testvoornaam = "fluppe" },
+                         new Test { id = 2, testnaam = "hendrickx", testvoornaam = "sven" } };
+            //var testje = new Test { id = 1, testnaam = "vermeersch", testvoornaam = "fluppe" };
+            return View(testje);
+
         }
     }
 }
---
+++ Projectwerk.Vermeersch.f/Models/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projectwerk.Vermeersch.f.Models
+{
+    public class Test
+    {
+        public int id { get; set; }
+        public string testnaam { get; set; }
+        public string testvoornaam { get; set; }
+    }
+}
