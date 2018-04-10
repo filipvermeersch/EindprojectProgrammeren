a98a3fa0bb477f8c17973ee9ac4286fb58413142
filip.vermeersch <filip.vermeersch@outlook.com>
2018-04-10 08:34:02 +0200

    Partial view resultaat

--- Projectwerk.Vermeersch.f/Controllers/ResultatenController.cs
+++ Projectwerk.Vermeersch.f/Controllers/ResultatenController.cs
@@ -1,4 +1,5 @@
 ﻿using Projectwerk.Vermeersch.f.Data;
+using Projectwerk.Vermeersch.f.Models;
 using Projectwerk.Vermeersch.f.Viewmodels;
 using System;
 using System.Collections.Generic;
@@ -13,18 +14,54 @@ namespace Projectwerk.Vermeersch.f.Controllers
         private TriatlonContext db = new TriatlonContext();
 
         [HttpGet]
-        public ActionResult AddResult(int id)
-        {
-            var wedstrijdId = 1;
-            var wedstrijd = db.Wedstrijden.Include("Sport").FirstOrDefault(w => w.Id == wedstrijdId);
-            ResultaatViewModel RVM = new ResultaatViewModel();
-            RVM.PutterId = id;
-            RVM.WedstrijdId = wedstrijdId;
-            
+        public ActionResult AddResult(string PutterId , string  Id)
 
+        {
+            int nummer = Convert.ToInt32(PutterId);
+            int wedstrijdNummer = Convert.ToInt32(Id);
+            var wedstrijd = db.Wedstrijden.Include("Sport").FirstOrDefault(w => w.Id == wedstrijdNummer);
+            var putter = db.Putters.FirstOrDefault(p => p.Id == nummer);
+            ResultaatViewModel RVM = new ResultaatViewModel();
+            RVM.PutterId = nummer;
+            RVM.WedstrijdId = wedstrijdNummer;
             RVM.Plaats = wedstrijd.Plaats;
             RVM.soortSport = wedstrijd.Sport.soortSport;
             return PartialView("_AddResult", RVM);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddResult(ResultaatViewModel RVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var resultaat = new Resultaat();
+                resultaat.UitslagAlgemeen = RVM.UitslagAlgemeen;
+                resultaat.UitslagCategorie = RVM.UitslagCategorie;
+                resultaat.WedstrijdId = RVM.WedstrijdId;
+                resultaat.PutterID = RVM.PutterId;
+
+                var putter = db.Putters.FirstOrDefault(p => p.Id == RVM.PutterId);
+                var wedstrijd = db.Wedstrijden.FirstOrDefault(w => w.Id == RVM.WedstrijdId);
+                db.Resultaten.Add(resultaat);
+                //wedstrijd.Resultaten.Add(resultaat);
+                //putter.Resultaten.Add(resultaat);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Wedstrijden", new { Id = RVM.PutterId });
+
+            }
+            return View(RVM);
+        }
+        public ActionResult Result(string id, string wedstrijdId)
+        {
+            int nummer = Convert.ToInt32(id);
+            int wedstrijdNummer = Convert.ToInt32(wedstrijdId);
+
+            var result = db.Resultaten.Include("Wedstrijd").FirstOrDefault(r => r.PutterID == nummer && r.WedstrijdId == wedstrijdNummer);
+
+            return PartialView("_Result", result);
 
         }
 
--- Projectwerk.Vermeersch.f/Viewmodels/ResultaatViewModel.cs
+++ Projectwerk.Vermeersch.f/Viewmodels/ResultaatViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,8 +13,12 @@ namespace Projectwerk.Vermeersch.f.Viewmodels
         public string soortSport { get; set; }
         public string Plaats { get; set; }
 
+        [Range(0, int.MaxValue, ErrorMessage = "Only integers are allowed")]
         public int UitslagCategorie { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Only integers are allowed")]
         public int UitslagAlgemeen { get; set; }
+
         public int PutterId { get; set; }
         public int WedstrijdId { get; set; }
 
